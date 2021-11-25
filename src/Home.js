54ee6781b8abb5a8ce05583a5aeb9258580abce0
Nathan Nguyen <micklebrain@gmail.com>
2021-11-25 13:45:05 -0500

    updates

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,10 @@ import logo from './logo.svg';
 
 function Home() {
     return (
-      <div className="App">      
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+      <div className="App">              
         <h1> Lost Minds </h1>
         <h2> Itinerary to travel USA cities so you don't waste time </h2>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
