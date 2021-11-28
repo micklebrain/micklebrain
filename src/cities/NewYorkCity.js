b254e06fcba424e431b2db681f7e126e1654a530
Nathan Nguyen <micklebrain@gmail.com>
2021-11-28 18:04:17 -0500

    style

--- src/cities/NewYorkCity.js
+++ src/cities/NewYorkCity.js
@@ -11,7 +11,6 @@ class NewYorkCity extends React.Component {
 
     render() {
         return (<div>
-
             <h2> Food delivery promo codes </h2>
             <ul>
                 <li> Uber eats promo code: eats-sxnvv </li>
@@ -19,22 +18,16 @@ class NewYorkCity extends React.Component {
                 <li> Seamless: https://www.seamless.com/referral/f170c1e0-4e7d-11ec-9f09-d3949d14d0dd?utm_source=seamless.com&utm_medium=content_owned&utm_campaign=growth_refer-a-friend_share-link&utm_content=promo_ </li>
             </ul>
 
-
             <Link to="/NYCarticle4" class="articlePreview"> <img class="article" src={nycboroughs} alt="Italian Trulli" /> Best resturant in every NYC borough </Link>
-
             <Link to="/NYCarticle2" class="articlePreview"> <img class="article" src={nycneighborhoods} alt="Best resturant in every NYC neighborhood " /> Best resturant in every NYC neighborhood </Link>
-
             <Link to="/NYCarticle" class="articlePreview"> <img class="article" src={nycsubway} alt="Best resturant by every subway stop" /> Best resturant by every subway stop </Link>
-
             <Link to="/NYCarticle3" class="articlePreview"> <img class="article" src={pie} alt="Italian Trulli" /> Stuck on where to eat? </Link>
-
 
             {/* <h2>  Top attractions </h2>
             <ul>
                 <li> https://www.hudsonyardsnewyork.com/discover/vessel </li>                
                 <li> https://www.esbnyc.com/ </li>
             </ul> */}
-
         </div>)
     }
 }
