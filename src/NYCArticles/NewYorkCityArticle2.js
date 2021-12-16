0127ed1ee09790fd64204405cc66cdf54268005c
Nathan Nguyen <micklebrain@gmail.com>
2021-12-16 15:44:32 -0500

    updates

--- src/NYCArticles/NewYorkCityArticle2.js
+++ src/NYCArticles/NewYorkCityArticle2.js
@@ -18,9 +18,9 @@ class NewYorkCityArticle2 extends React.Component {
     render() {            
         return (<div>       
             <div>     
-                <h1> Best resturant in every Manhattan neighborhood </h1>       
-                <h2> Alphabet City -  </h2>
-                <h2> Battery Park City - Dig </h2>
+                <h1> Best resturant in every Manhattan neighborhood </h1>                   
+                <a href="https://www.grubhub.com/restaurant/raclette-195-avenue-a-new-york/305505" target="_blank"> Alphabet City - Raclette </a>
+                <a href="https://www.grubhub.com/restaurant/dig---madison-square-16-e-23rd-st-new-york/304579" target="_blank"> Battery Park City - Dig </a>
                 <h2> Central Park - Express Cafe in the Loeb Boathouse </h2>
                 <h2> Chelsea - Bottino </h2>
                 <h2> Chinatown - Kong Sihk Tong </h2>
@@ -28,35 +28,35 @@ class NewYorkCityArticle2 extends React.Component {
                 <h2> East Harlem - Texas Chicken and Burgers </h2>
                 <h2> East Village - Nowan </h2>
                 <h2> Financial District - Bao Bao </h2>
-                <h2> Flatiron District - </h2>
+                <h2> Flatiron District - Upland </h2>
                 <h2> Garment district - Chef Yu </h2>
                 <h2> Gramercy - Bite </h2>
                 <h2> Greenwich village - Tue Thai Food </h2>                
                 <h2> Harlem - Manna's </h2>
-                <h2> Hell's Kitchen - </h2>
-                <h2> Hudson Square - </h2>
-                <h2> Inwood - </h2>
-                <h2> Kips Bay - </h2>
+                <h2> Hell's Kitchen - Yum Yum Too </h2>
+                <h2> Hudson Square - Ear Inn </h2>
+                <h2> Inwood - La Casa del Mofongo and Piano Bar </h2>
+                <h2> Kips Bay - Soft Swerve </h2>
                 <h2> Koreatown - Antoya KBBQ </h2>
                 <h2> Lower East Side - Sweet Chick </h2>
                 <h2> Little Italy - Gelso and Grand </h2>
-                <h2> Meatpacking District - </h2>
+                <h2> Meatpacking District - Catch NYC </h2>
                 <h2> Midtown East - Obao </h2>
-                <h2> Midtown South - </h2>
+                <h2> Midtown South - Chef Yu </h2>
                 <h2> Midtown West - Ivan Ramen Slurp Shop </h2>
                 <h2> Morning Side Heights - Tom's </h2>
                 <h2> Murray Hill - Sushi Ryusei </h2>
-                <h2> NoHo - </h2>
-                <h2> Nolita - </h2>
+                <h2> NoHo - Lafayette Grand Café and Bakery </h2>
+                <h2> Nolita - Rubirosa </h2>
                 <h2> NOMAD - KazuNori </h2>
-                <h2> Roosevelt Island - </h2>
+                <h2> Roosevelt Island - Granny Annie's Bar and Kitchen </h2>
                 <h2> SOHO - Fanelli’s Cafe </h2>
                 <h2> Stuyvesant Town - Ess-a-Bagel </h2>
                 <h2> Tribeca - Bubby's </h2>                
-                <h2> Union Square - </h2>
+                <h2> Union Square - Breads Bakery </h2>
                 <h2> Upper East Side - Vietnaam </h2>
                 <h2> Upper West Side - Sushi Kaito </h2>
-                <h2> West Village - </h2>
+                <h2> West Village - L'Artusi </h2>
             </div>    
         </div>)
     }
--- src/cities/NewYorkCity.js
+++ src/cities/NewYorkCity.js
@@ -12,7 +12,7 @@ class NewYorkCity extends React.Component {
     render() {
         return (<div>
             <h1> New York City travel guide </h1>
-            <Link to="/NYCarticle4" class="articlePreview"> <img class="article" src={nycboroughs} alt="Italian Trulli" /> <span> Best resturant in all five NYC boroughs </span> </Link>
+            <Link to="/NYCarticle4" class="articlePreview"> <img class="article" src={nycboroughs} alt="Italian Trulli" /> Best resturant in all five NYC boroughs </Link>
             <Link to="/NYCarticle2" class="articlePreview"> <img class="article" src={nycneighborhoods} alt="Best resturant in every NYC neighborhood " /> Best resturant in every NYC neighborhood </Link>
             <Link to="/NYCarticle" class="articlePreview"> <img class="article" src={nycsubway} alt="Best resturant by every subway stop" /> Best resturant by every subway stop </Link>
             {/* <Link to="/NYCarticle3" class="articlePreview"> <img class="article" src={pie} alt="Italian Trulli" /> Stuck on where to eat? </Link> */}
