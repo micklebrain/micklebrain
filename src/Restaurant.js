83741b47705c6f7ade6dd077d63845791647b360
Nathan Nguyen <micklebrain@gmail.com>
2022-01-20 22:55:52 -0500

    updates

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -5,6 +5,7 @@ import './Restaurant.css'
 import grubhubIcon from './images/grubhubIcon.png'
 import postmatesIcon from './images/Postmates_Icon.png'
 import resyIcon from './images/resy_icon.png'
+import openTableIcon from './images/open-table-icon.png'
 import uberEatsIcon from './images/Uber-Eats-Icon.png'
 
 import imageName from './images/imageName.jpeg'
@@ -28,8 +29,9 @@ class Restaurant extends React.Component {
             <img class="articleImage" src={imageName} alt="image" />
             <h2> {this.state.name} </h2>
             <h3> {this.state.address} </h3>
-            <h4> Recommened dish - {this.state.recommendedDish} </h4>
+            <h4> Recommened dish - {this.state.recommendedDish} </h4>            
             <div class="tag"> {this.state.neighborhood} </div>
+            <h4> Available for Delivery/Takeout </h4>
             <div class="row">
                 <div class="column">
                     <a href={''} target="_blank"> <img src={grubhubIcon} alt="Gruhbhub" width="50" height="50" /> </a>
@@ -40,8 +42,14 @@ class Restaurant extends React.Component {
                 <div class="column">
                     <a href={''} target="_blank"> <img src={uberEatsIcon} alt="Ubereats" width="50" height="50" /> </a>
                 </div>
+            </div>
+            <h4> Available for Reservations </h4>
+            <div class="row">
                 <div class="column">
                     <a href={''} target="_blank"> <img src={resyIcon} alt="Resy" width="50" height="50" /> </a>
+                </div>
+                <div class="column">
+                    <a href={''} target="_blank"> <img src={openTableIcon} alt="Open Table Icon" width="50" height="50" /> </a>
                 </div>
             </div>
         </div>)
