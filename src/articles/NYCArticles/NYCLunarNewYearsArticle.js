77a135202145db4f0c4a34459b87765f7f785be9
Nathan Nguyen <micklebrain@gmail.com>
2022-01-31 16:26:58 -0500

    updates

--- src/articles/NYCArticles/NYCLunarNewYearsArticle.js
+++ src/articles/NYCArticles/NYCLunarNewYearsArticle.js
@@ -14,8 +14,10 @@ class NYCLunarNewYearsArticle extends React.Component {
                 <ul>
                     {/* <button onClick={this.addToItinerary.bind(this)}> Add to itinerary </button> */}
                     <li> Feb 1st @ 12am | Lunar New Years Firecracker Ceremony Sara D. Roosevelt Park </li>
-                    <li> Feb 4 from 7:30 - 10:30pm | Macy and Ricky Present: NYC Lunar New Year Comedy Show  </li>
-                    <li> Feb 12 from 1-5pm | Chinatown Lunar New Year Parade and Festival </li>                    
+                    <li> Feb 3-5 @ 4-6pm | Lunar new years celebration, live ice carving | @ Brookfield Place </li>
+                    <li> Feb 4 @ 7:30-10:30pm | Macy and Ricky Present: NYC Lunar New Year Comedy Show | @ St. Marks Comedy Club </li>
+                    <li> Feb 5 | Lunar New Years Celebration | @ Queens Botanical Garden </li>
+                    <li> Feb 12 from 1-5pm | Chinatown Lunar New Year Parade and Festival </li>
                 </ul>
             </div>)
     }
