4917d97d15de34f3e17e9eef5c00360bb411bcdf
Nathan Nguyen <micklebrain@gmail.com>
2022-02-03 19:42:33 -0500

    updates

--- src/articles/NYCArticles/NYCLunarNewYearsArticle.js
+++ src/articles/NYCArticles/NYCLunarNewYearsArticle.js
@@ -13,7 +13,6 @@ class NYCLunarNewYearsArticle extends React.Component {
                 <h1> Top things in NYC for Lunar New Years celebration </h1>
                 <ul>
                     {/* <button onClick={this.addToItinerary.bind(this)}> Add to itinerary </button> */}
-                    <li> Feb 1st @ 12am | Lunar New Years Firecracker Ceremony Sara D. Roosevelt Park </li>
                     <li> Feb 3-5 @ 4-6pm | Lunar new years celebration, live ice carving | @ Brookfield Place </li>
                     <li> Feb 4 @ 7:30-10:30pm | Macy and Ricky Present: NYC Lunar New Year Comedy Show | @ St. Marks Comedy Club </li>
                     <li> Feb 5 | Lunar New Years Celebration | @ Queens Botanical Garden </li>
--- src/articles/NYCArticles/NYCUpcomingEventsArticle.js
+++ src/articles/NYCArticles/NYCUpcomingEventsArticle.js
@@ -19,10 +19,17 @@ class NYCUpcomingEventsArticle extends React.Component {
             <div>
                 <h1> Upcoming Events </h1>
                 <ul>
-                    {/* <button onClick={this.addToItinerary.bind(this)}> Add to itinerary </button> */}                    
-                    <li> Feb 3-5 @ 4-6pm | Lunar new years celebration, live ice carving | @ Brookfield Place </li>
+                    {/* <button onClick={this.addToItinerary.bind(this)}> Add to itinerary </button> */}
+                    <li> ongoing @ 6-10pm | Free Thursdays admissions | Brooklyn Muesuem </li> 
+                    <li> ongoing | Free Friday admissions | The Rubin museum of art </li>                              
+                    <li> Feb 3-5 @ 4-6pm | Lunar new years celebration, live ice carving | @ Brookfield Place </li>                    
                     <li> Feb 4 @ 7:30-10:30pm | Macy and Ricky Present: NYC Lunar New Year Comedy Show | @ St. Marks Comedy Club </li>
                     <li> Feb 5 | Lunar New Years Celebration | @ Queens Botanical Garden </li>
+                    <li> March 4 | Rezz | Terminal 5 </li>
+                    <li> March 6 | Seong-Jin | New Jersey Performing Arts Center </li>
+                    <li> March 18 | Reliant K | Irving plaza </li>
+                    <li> March 27 | New York Rangers vs Buffalo Sabres | The Madison Square Garden </li>
+                    <li> March 28 | New York Knicks vs Chicago Bull | The Madison Square Garden </li>
                 </ul>
             </div>)
     }
