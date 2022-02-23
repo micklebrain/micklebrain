415da8513aac20f280ae03c5f871817d69b0d31d
Nathan Nguyen <micklebrain@gmail.com>
2022-02-23 17:16:36 -0500

    updates

--- src/articles/NYCArticles/NYCUpcomingEventsArticle.js
+++ src/articles/NYCArticles/NYCUpcomingEventsArticle.js
@@ -19,16 +19,19 @@ class NYCUpcomingEventsArticle extends React.Component {
                     <h2> All year </h2>
                     <li> ongoing @ 6-10pm | Free Thursdays admissions | @Brooklyn Muesuem </li> 
                     <li> ongoing | Free Friday admissions @6-10pm | @The Rubin Museum of Art </li>
-                    <li> ongoing | Free Friday admissions | @The Morgan library and museum </li>
-                    <li> Jan 8 - Feb 27 | FirstImpressions | West Chelsea Contemporary </li>
+                    <li> ongoing | Free Friday admissions | @The Morgan library and museum </li>                    
+
                     <h2> Febuary </h2>                    
                     <li> through Feb 21 | Broadway 2 for 1 |</li>
+                    <li> through Feb 27 | First Impressions | West Chelsea Contemporary </li>
+
                     <h2> March </h2>
                     <li> March 4 | Rezz | @Terminal 5 </li>
                     <li> March 6 | Seong-Jin | @New Jersey Performing Arts Center </li>
                     <li> March 18 | Reliant K | @Irving plaza </li>
                     <li> March 27 | New York Rangers vs Buffalo Sabres | @The Madison Square Garden </li>
                     <li> March 28 | New York Knicks vs Chicago Bull | @The Madison Square Garden </li>
+                    
                     <h2> April </h2>
                     <li> Apr 8 | Lil Tecca | @Terminal 5 </li>
                 </ul>
