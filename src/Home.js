a1e6348f35ebfb0ce451ced4e7d753f08b33cc80
Nathan Nguyen <micklebrain@gmail.com>
2022-03-01 18:03:54 -0500

    updates

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,12 @@ function Home() {
   return (
     <div className="App">
       <h1> Links </h1>
-      <div class='link'> <h1> <a href="https://github.com/micklebrain"> Github repositories </a> </h1> </div>
-      <div class='link'> <h1> <a href="https://www.linkedin.com/in/nathanthainguyen/"> Linkedin </a> </h1> </div> 
-      <div class='link'>  <h1> <a href="https://opensea.io/collection/micklebrain"> Opensea NFT Collection </a> </h1> </div>
-      <div class='link'> <h1> <a href="https://www.patreon.com/micklebrain"> Patreon </a> </h1> </div>
-      <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain"> Poshmark closet </a> </h1> </div>
-      <div class='link'> <h1> <a href="https://twitter.com/micklebrain"> Twitter </a> </h1> </div>
+      <div class='link'> <h1> <a href="https://github.com/micklebrain" target="_blank"> Github repositories </a> </h1> </div>
+      <div class='link'> <h1> <a href="https://www.linkedin.com/in/nathanthainguyen/" target="_blank"> Linkedin </a> </h1> </div> 
+      <div class='link'>  <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank"> Opensea NFT Collection </a> </h1> </div>
+      <div class='link'> <h1> <a href="https://www.patreon.com/micklebrain" target="_blank"> Patreon </a> </h1> </div>
+      <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain" target="_blank"> Poshmark closet </a> </h1> </div>
+      <div class='link'> <h1> <a href="https://twitter.com/micklebrain" target="_blank"> Twitter </a> </h1> </div>
     </div>
   );
 }
