fcc31d715b7422df335d11a07c466b4be20e38c9
Nathan Nguyen <micklebrain@gmail.com>
2022-04-05 22:33:41 -0400

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 
-import github from './images/github.png';
 import linkedin from './images/linkedin.png';
 import opensea from './images/opensea.png';
 import patreon from './images/patreon.png';
@@ -12,8 +11,7 @@ import twitter from './images/twitter.png';
 function Home() {
   return (
     <div className="App">
-      <h1> Links </h1>
-      <div class='link'> <h1> <a href="https://github.com/micklebrain" target="_blank"><img class="icon" src={github} alt="github"/>Github repositories</a></h1></div>
+      <h1> Links </h1>      
       <div class='link'> <h1> <a href="https://www.linkedin.com/in/nathanthainguyen/" target="_blank">Linked<img class="icon" src={linkedin} alt="linkedin"/></a> </h1> </div>
       <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank"><img class="icon" src={opensea} alt="opensea"/>pensea NFT</a> </h1> </div>
       <div class='link'> <h1> <a href="https://www.patreon.com/micklebrain" target="_blank"><img class="icon" src={patreon} alt="patreon"/>atreon</a> </h1> </div>
