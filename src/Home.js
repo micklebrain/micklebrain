522878d800a3dd8b1afd90bd373a8bbf1b18bbd7
Nathan Nguyen <micklebrain@gmail.com>
2022-04-16 11:43:31 -0400

    updates

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import linkedin from './images/linkedin.png';
 import opensea from './images/opensea.png';
 import patreon from './images/patreon.png';
+import pinterest from './images/pinterest.png';
 import poshmark from './images/poshmark.png';
 import tiktok from './images/tiktok.png';
 import tumblr from './images/tumblr.png';
@@ -15,6 +16,7 @@ function Home() {
       <div class='link'> <h1> <a href="https://www.linkedin.com/in/nathanthainguyen/" target="_blank">Linked<img class="icon" src={linkedin} alt="linkedin"/></a> </h1> </div>
       <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank"><img class="icon" src={opensea} alt="opensea"/>pensea NFT</a> </h1> </div>
       <div class='link'> <h1> <a href="https://www.patreon.com/micklebrain" target="_blank"><img class="icon" src={patreon} alt="patreon"/>atreon</a> </h1> </div>
+      <div class='link'> <h1> <a href="https://pin.it/1dPIGAq" target="_blank"><img class="icon" src={pinterest} alt="patreon"/>interest</a> </h1> </div>    
       <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain" target="_blank"><img class="icon" src={poshmark} alt="poshmark"/>Poshmark closet</a> </h1> </div>
       <div class='link'> <h1> <a href="https://www.tiktok.com/@micklebrain" target="_blank"><img class="icon" src={tiktok} alt="tikTok"/>TikTok</a> </h1> </div>
       <div class='link'> <h1> <a href="https://www.tumblr.com/blog/micklebrain" target="_blank"><img class="icon" src={tumblr} alt="tumbler"/>umblr</a> </h1> </div>      
