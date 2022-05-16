00526116dda5847c9d4c54d539aee68fbfeb8316
Nathan Nguyen <micklebrain@gmail.com>
2022-05-16 02:14:25 -0400

    updates

--- src/Home.js
+++ src/Home.js
@@ -15,18 +15,28 @@ function Home() {
   return (
     <div className="App">
       <h2> Connect </h2>
-      <div class='link'> <h1> <a href="https://onlyfans.com/micklebrain" target="_blank"><img class="icon" src={onlyfans} alt="twitter"/>OnlyFans</a> </h1> </div>
-      <div class='link'> <h1> <a href="https://www.instagram.com/micklebrain" target="_blank"><img class="icon" src={instagram} alt="instagram"/>Instagram</a> </h1> </div>
-      <div class='link'> <h1> <a href="https://www.linkedin.com/in/nathanthainguyen" target="_blank">Linked<img class="icon" src={linkedin} alt="linkedin"/></a> </h1> </div>
-      <div class='link'> <h1> <a href="https://twitter.com/micklebrain" target="_blank"><img class="icon" src={twitter} alt="twitter"/>Twitter</a> </h1> </div>      
+      <div class='link'> <h1> <a href="https://onlyfans.com/micklebrain" target="_blank">OnlyFans</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://onlyfans.com/micklebrain" target="_blank"><img class="icon" src={onlyfans} alt="twitter"/>OnlyFans</a> </h1> </div> */}
+      <div class='link'> <h1> <a href="https://www.instagram.com/micklebrain" target="_blank">Instagram</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://www.instagram.com/micklebrain" target="_blank"><img class="icon" src={instagram} alt="instagram"/>Instagram</a> </h1> </div> */}
+      <div class='link'> <h1> <a href="https://www.linkedin.com/in/nathanthainguyen" target="_blank">LinkedIn</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://www.linkedin.com/in/nathanthainguyen" target="_blank">Linked<img class="icon" src={linkedin} alt="linkedin"/></a> </h1> </div> */}
+      <div class='link'> <h1> <a href="https://twitter.com/micklebrain" target="_blank">Twitter</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://twitter.com/micklebrain" target="_blank"><img class="icon" src={twitter} alt="twitter"/>Twitter</a> </h1> </div> */}
       <h2> Inspiration </h2>
-      <div class='link'> <h1> <a href="https://www.patreon.com/micklebrain" target="_blank"><img class="icon" src={patreon} alt="patreon"/>atreon</a> </h1> </div>            
-      <div class='link'> <h1> <a href="https://pin.it/1dPIGAq" target="_blank"><img class="icon" src={pinterest} alt="patreon"/>interest</a> </h1> </div>          
-      <div class='link'> <h1> <a href="https://www.tiktok.com/@micklebrain" target="_blank"><img class="icon" src={tiktok} alt="tikTok"/>TikTok</a> </h1> </div>
-      <div class='link'> <h1> <a href="https://www.tumblr.com/blog/micklebrain" target="_blank"><img class="icon" src={tumblr} alt="tumbler"/>umblr</a> </h1> </div>
+      <div class='link'> <h1> <a href="https://www.patreon.com/micklebrain" target="_blank">Patreon</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://www.patreon.com/micklebrain" target="_blank"><img class="icon" src={patreon} alt="patreon"/>atreon</a> </h1> </div> */}
+      <div class='link'> <h1> <a href="https://pin.it/1dPIGAq" target="_blank">Pinterest</a> </h1> </div>          
+      {/* <div class='link'> <h1> <a href="https://pin.it/1dPIGAq" target="_blank"><img class="icon" src={pinterest} alt="patreon"/>interest</a> </h1> </div>           */}
+      <div class='link'> <h1> <a href="https://www.tiktok.com/@micklebrain" target="_blank">TikTok</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://www.tiktok.com/@micklebrain" target="_blank"><img class="icon" src={tiktok} alt="tikTok"/>TikTok</a> </h1> </div> */}
+      <div class='link'> <h1> <a href="https://www.tumblr.com/blog/micklebrain" target="_blank">Tumblr</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://www.tumblr.com/blog/micklebrain" target="_blank"><img class="icon" src={tumblr} alt="tumbler"/>umblr</a> </h1> </div> */}
       <h2> Shop </h2>
-      <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank"><img class="icon" src={opensea} alt="opensea"/>pensea NFT</a> </h1> </div>
-      <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain" target="_blank"><img class="icon" src={poshmark} alt="poshmark"/>Poshmark closet</a> </h1> </div>
+      <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank">Opensea NFT</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank"><img class="icon" src={opensea} alt="opensea"/>pensea NFT</a> </h1> </div> */}
+      <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain" target="_blank">Poshmark closet</a> </h1> </div>
+      {/* <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain" target="_blank"><img class="icon" src={poshmark} alt="poshmark"/>Poshmark closet</a> </h1> </div> */}
     </div>
   );
 }
