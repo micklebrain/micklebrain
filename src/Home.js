d83f59019316b25f19dadfadb46e0d02b7bab38d
Nathan Nguyen <micklebrain@gmail.com>
2022-05-16 02:15:50 -0400

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 
-import instagram from './images/instagram.png';
-import linkedin from './images/linkedin.png';
-import opensea from './images/opensea.png';
-import onlyfans from './images/onlyfans.png';
-import patreon from './images/patreon.png';
-import pinterest from './images/pinterest.png';
-import poshmark from './images/poshmark.png';
-import tiktok from './images/tiktok.png';
-import tumblr from './images/tumblr.png';
-import twitter from './images/twitter.png';
+// import instagram from './images/instagram.png';
+// import linkedin from './images/linkedin.png';
+// import opensea from './images/opensea.png';
+// import onlyfans from './images/onlyfans.png';
+// import patreon from './images/patreon.png';
+// import pinterest from './images/pinterest.png';
+// import poshmark from './images/poshmark.png';
+// import tiktok from './images/tiktok.png';
+// import tumblr from './images/tumblr.png';
+// import twitter from './images/twitter.png';
 
 function Home() {
   return (
