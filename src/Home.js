a3ddc3b800eac79ea97063f2f2d979d4f11f1e09
Nathan Nguyen <micklebrain@gmail.com>
2023-09-15 14:50:13 +0900

    update

--- src/Home.js
+++ src/Home.js
@@ -34,7 +34,7 @@ function Home() {
       {/* <div class='link'> <h1> <a href="https://www.tiktok.com/@micklebrain" target="_blank"><img class="icon" src={tiktok} alt="tikTok"/>TikTok</a> </h1> </div> */}
       {/* <div class='link'> <h1> <a href="https://www.tumblr.com/blog/micklebrain" target="_blank">Tumblr</a> </h1> </div> */}
       {/* <div class='link'> <h1> <a href="https://www.tumblr.com/blog/micklebrain" target="_blank"><img class="icon" src={tumblr} alt="tumbler"/>umblr</a> </h1> </div> */}      
-      <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank">Opensea NFT</a> </h1> </div>
+      <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank">NFTs</a> </h1> </div>
       {/* <div class='link'> <h1> <a href="https://opensea.io/collection/micklebrain" target="_blank"><img class="icon" src={opensea} alt="opensea"/>pensea NFT</a> </h1> </div> */}
       <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain" target="_blank">Fashion</a> </h1> </div>
       {/* <div class='link'> <h1> <a href="https://poshmark.com/closet/micklebrain" target="_blank"><img class="icon" src={poshmark} alt="poshmark"/>Poshmark closet</a> </h1> </div> */}
