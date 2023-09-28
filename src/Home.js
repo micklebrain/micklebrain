e3cbc93da15317facff92a219cb36a25323aa7e1
Nathan Nguyen <micklebrain@gmail.com>
2023-09-28 12:20:52 +0900

    update

--- src/Home.js
+++ src/Home.js
@@ -16,6 +16,7 @@ function Home() {
     <div className="App">   
       <div class='link'> <h1> <a href="https://onlyfans.com/micklebrain" target="_blank">Foot Model</a> </h1> </div>
       <div class='link'> <h1 class='button-49'> <a href="https://www.tiktok.com/@micklebrain" target="_blank">TikTok</a> </h1> </div>
+      <h3>music</h3>
       <div class='link'> <h1 class='button-49'> <a href="https://soundcloud.com/micklebrain" target="_blank">SoundCloud</a> </h1> </div>
       <div class='link'> <h1 class='button-49'> <a href="https://music.apple.com/us/artist/micklebrain/1709188087" target="_blank">AppleMusic</a> </h1> </div>
       {/* <div class='link'> <h1> <a href="https://onlyfans.com/micklebrain" target="_blank"><img class="icon" src={onlyfans} alt="twitter"/>OnlyFans</a> </h1> </div> */}
