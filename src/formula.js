b123959e0cbd009b6a7cd95e97c5e69789ee27e6
Nathan Nguyen <micklebrain@gmail.com>
2024-10-31 16:07:54 -0700

    u
    u

--- src/formula.js
+++ src/formula.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 function Formula() {
   return (
     <div>
-      <h1>Net Worth: $270,000,000 | 10/31/24 | next goal: $300,000</h1>
-      <h1># Twitch followers: 2 | 10/31/24</h1>
-      <h1># Youtube subscribers: 3 | 10/31/24</h1>
-      <h1># Instagram followers: 17,500 | 10/31/24</h1>
-      <h1># TikTok followers: 228 | 10/31/24 | next goal: 100,000</h1>
-      <h1>height: 5'7“ | 10/31/24</h1>
-      <h1># time contracted incurable Hepatitis B: 0 | 10/31/24</h1>
-      <h1># time contracted incurable Herpes: 0 | 10/31/24</h1>
-      <h1># time contracted incurable HIV: 0 | 10/31/24</h1>
-      <h1># time contracted incurable HPV: 0 | 10/31/24</h1>
+      <h1>$270,000,000 net worth | 10/31/24 | next goal: $300,000</h1>
+      <h1>2 Twitch followers | 10/31/24</h1>
+      <h1>3 Youtube subscribers | 10/31/24</h1>
+      <h1>17,500 Instagram followers| 10/31/24</h1>
+      <h1>228 TikTok followers | 10/31/24 | next goal: 100,000</h1>
+      <h1>5'7“ height | 10/31/24</h1>
+      <h1>0 times contracted incurable Hepatitis B | 10/31/24</h1>
+      <h1>0 times contracted incurable Herpes | 10/31/24</h1>
+      <h1>0 times contracted incurable HIV | 10/31/24</h1>
+      <h1>0 times contracted incurable HPV | 10/31/24</h1>
       {/* <label for="pushups">Pushups completed today:</label> */}
       {/* <input type="number" name="pushups" step="1"></input> */}
     </div>
