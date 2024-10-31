8e629fdb6cac0bd1497c2d31fa3229c41d0c9de7
Nathan Nguyen <micklebrain@gmail.com>
2024-10-31 11:04:54 -0700

    Update formula.js
    u

--- src/formula.js
+++ src/formula.js
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 function Formula() {
   return (
     <div>
-      <h1>Net Worth: 250k</h1>
-      <h1>Twitch followers: 2</h1>
-      <h1>Instagram followers: 17.5k</h1>
-      <h1>TikTok followers: 228</h1>
+      <h1>Net Worth: $250,000,000 | 10/31/24</h1>
+      <h1># Twitch followers: 2 | 10/31/24</h1>
+      <h1># Youtube subscribers: 3 | 10/31/24</h1>
+      <h1># Instagram followers: 17,500 | 10/31/24</h1>
+      <h1># TikTok followers: 228 | 10/31/24</h1>      
+      <h1># time contracted incurable Hepatitis B: 0</h1>
+      <h1># time contracted incurable Herpes: 0</h1>
+      <h1># time contracted incurable HIV: 0</h1>
+      <h1># time contracted incurable HPV: 0</h1>
     </div>
   );
 }
