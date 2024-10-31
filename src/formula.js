a67e843d7e8faa0a887415978934d6619508d115
Nathan Nguyen <micklebrain@gmail.com>
2024-10-31 13:06:13 -0700

    u
    u

--- src/formula.js
+++ src/formula.js
@@ -7,11 +7,14 @@ function Formula() {
       <h1># Twitch followers: 2 | 10/31/24</h1>
       <h1># Youtube subscribers: 3 | 10/31/24</h1>
       <h1># Instagram followers: 17,500 | 10/31/24</h1>
-      <h1># TikTok followers: 228 | 10/31/24</h1>      
+      <h1># TikTok followers: 228 | 10/31/24</h1>
+      <h1>height: 5'7“ | 10/31/24</h1>
       <h1># time contracted incurable Hepatitis B: 0</h1>
       <h1># time contracted incurable Herpes: 0</h1>
       <h1># time contracted incurable HIV: 0</h1>
       <h1># time contracted incurable HPV: 0</h1>
+      {/* <label for="pushups">Pushups completed today:</label> */}
+      {/* <input type="number" name="pushups" step="1"></input> */}
     </div>
   );
 }
