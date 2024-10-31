42cdd135ef9a9dccead1c47f9c2b16ce00450799
Nathan Nguyen <micklebrain@gmail.com>
2024-10-31 15:31:50 -0700

    u
    u

--- src/formula.js
+++ src/formula.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 function Formula() {
   return (
     <div>
-      <h1>Net Worth: $250,000,000 | 10/31/24</h1>
+      <h1>Net Worth: $270,000,000 | 10/31/24 | next goal: $300,000</h1>
       <h1># Twitch followers: 2 | 10/31/24</h1>
       <h1># Youtube subscribers: 3 | 10/31/24</h1>
       <h1># Instagram followers: 17,500 | 10/31/24</h1>
-      <h1># TikTok followers: 228 | 10/31/24</h1>
+      <h1># TikTok followers: 228 | 10/31/24 | next goal: 100,000</h1>
       <h1>height: 5'7“ | 10/31/24</h1>
-      <h1># time contracted incurable Hepatitis B: 0</h1>
-      <h1># time contracted incurable Herpes: 0</h1>
-      <h1># time contracted incurable HIV: 0</h1>
-      <h1># time contracted incurable HPV: 0</h1>
+      <h1># time contracted incurable Hepatitis B: 0 | 10/31/24</h1>
+      <h1># time contracted incurable Herpes: 0 | 10/31/24</h1>
+      <h1># time contracted incurable HIV: 0 | 10/31/24</h1>
+      <h1># time contracted incurable HPV: 0 | 10/31/24</h1>
       {/* <label for="pushups">Pushups completed today:</label> */}
       {/* <input type="number" name="pushups" step="1"></input> */}
     </div>
