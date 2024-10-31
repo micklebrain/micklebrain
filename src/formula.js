dbf66903584c9dcf4a78fefcef18ea2ef7765d3a
Nathan Nguyen <micklebrain@gmail.com>
2024-10-31 09:01:47 -0700

    Update formula.js
    update

--- src/formula.js
+++ src/formula.js
@@ -1,16 +1,5 @@
 import { Link } from "react-router-dom";
 
-// import instagram from './images/instagram.png';
-// import linkedin from './images/linkedin.png';
-// import opensea from './images/opensea.png';
-// import onlyfans from './images/onlyfans.png';
-// import patreon from './images/patreon.png';
-// import pinterest from './images/pinterest.png';
-// import poshmark from './images/poshmark.png';
-// import tiktok from './images/tiktok.png';
-// import tumblr from './images/tumblr.png';
-// import twitter from './images/twitter.png';
-
 function Formula() {
   return (
     <div>   
@@ -19,4 +8,4 @@ function Formula() {
   );
 }
 
-export default Home;
+export default Formula;
