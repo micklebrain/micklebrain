c28eea72c2f21aa0ba2f825b19f257a1a1409b56
Nathan Nguyen <micklebrain@gmail.com>
2024-11-03 00:59:18 -0700

    u

--- src/formula.js
+++ src/formula.js
@@ -5,20 +5,20 @@ import { Link } from "react-router-dom";
 function Formula() {
   return (
     <div>
-      
-      <p><span class='stat'>$270,000,000 net worth</span> next goal: $300,000</p>
-      <p><span class='stat'>400</span> SoLo loans funded | 10/31/24</p>
-      <p><span class='stat'>2</span> Twitch followers | 10/31/24</p>
-      <p><span class='stat'>3</span> Youtube subscribers | 10/31/24</p>
-      <p><span class='stat'>17,500</span> Instagram followers| 10/31/24 | next goal: 50,000</p>
-      <p><span class='stat'>228</span> TikTok followers | 10/31/24 | next goal: 100,000</p>
-      <p><span class='stat'>5</span> N JLPT level</p>
-      <p><span class='stat'>0</span> times contracted incurable Hepatitis B | 10/31/24</p>
-      <p><span class='stat'>0</span> times contracted incurable Herpes | 10/31/24</p>
-      <p><span class='stat'>0</span> times contracted incurable HIV | 10/31/24</p>
-      <p><span class='stat'>0</span> times contracted incurable HPV | 10/31/24</p>
-      <p><span class='stat'>28</span> years old</p>
-      <p><span class='stat'>5'7“</span> height | 10/31/24</p>
+      <p><span class='stat-good'>$270,000,000 net worth</span> next goal: $300,000</p>
+      <p><span class='stat-good'>400</span> SoLo loans funded | 10/31/24</p>
+      <p><span class='stat-bad'>2</span> Twitch followers | 10/31/24</p>
+      <p><span class='stat-bad'>3</span> Youtube subscribers | 10/31/24</p>
+      <p><span class='stat-bad'>17,500</span> Instagram followers| 10/31/24 | next goal: 50,000</p>
+      <p><span class='stat-bad'>228</span> TikTok followers | 10/31/24 | next goal: 100,000</p>
+      <p><span class='stat-bad'>5</span> N JLPT level </p>
+      <p> <span class='stat-bad'>1</span> TOPIK level</p>
+      <p><span class='stat-good'>0</span> times contracted incurable Hepatitis B | 10/31/24</p>
+      <p><span class='stat-good'>0</span> times contracted incurable Herpes | 10/31/24</p>
+      <p><span class='stat-good'>0</span> times contracted incurable HIV | 10/31/24</p>
+      <p><span class='stat-good'>0</span> times contracted incurable HPV | 10/31/24</p>
+      <p><span class='stat-neutral'>28</span> years old</p>
+      <p><span class='stat-neutral'>5'7“</span> height | 10/31/24</p>
     </div>
   );
 }
