35449e369ef7eb77530d157dc7efffae884de0f6
Nathan Nguyen <micklebrain@gmail.com>
2024-11-03 01:42:43 -0700

    u

--- src/formula.js
+++ src/formula.js
@@ -7,17 +7,18 @@ function Formula() {
     <div>
       <p><span class='stat-good'>$270,000,000 net worth</span> next goal: $300,000</p>
       <p><span class='stat-good'>400</span> SoLo loans funded | 10/31/24</p>
+      <p><span class='stat-bad'>290</span> <span class='small'>/3000</span> stocks owned | 10/31/24 | next goal: 100,000</p>
       <p><span class='stat-bad'>2</span> Twitch followers | 10/31/24</p>
       <p><span class='stat-bad'>3</span> Youtube subscribers | 10/31/24</p>
       <p><span class='stat-bad'>17,500</span> Instagram followers| 10/31/24 | next goal: 50,000</p>
       <p><span class='stat-bad'>228</span> TikTok followers | 10/31/24 | next goal: 100,000</p>
-      <p><span class='stat-bad'>5</span> N JLPT level </p>
-      <p> <span class='stat-bad'>1</span> TOPIK level</p>
+      <p><span class='stat-bad'>5</span> <span class='small'>4 3 2 1</span> N JLPT level </p>
+      <p> <span class='stat-bad'>1</span> <span class='small'>2 3 4 5</span> TOPIK level</p>
       <p><span class='stat-good'>0</span> times contracted incurable Hepatitis B | 10/31/24</p>
       <p><span class='stat-good'>0</span> times contracted incurable Herpes | 10/31/24</p>
       <p><span class='stat-good'>0</span> times contracted incurable HIV | 10/31/24</p>
       <p><span class='stat-good'>0</span> times contracted incurable HPV | 10/31/24</p>
-      <p><span class='stat-neutral'>28</span> years old</p>
+      <p><span class='stat-neutral'>29</span> years old</p>
       <p><span class='stat-neutral'>5'7“</span> height | 10/31/24</p>
     </div>
   );
