b85b71ea5e7475ccf4a6de6b1766e2602a9613e6
Nathan Nguyen <micklebrain@gmail.com>
2024-11-03 00:52:06 -0700

    u

--- src/formula.js
+++ src/formula.js
@@ -5,19 +5,20 @@ import { Link } from "react-router-dom";
 function Formula() {
   return (
     <div>
-      <h1>$270,000,000 net worth | 10/31/24 | next goal: $300,000</h1>
-      <h1>400 SoLo loans funded | 10/31/24</h1>
-      <h1>2 Twitch followers | 10/31/24</h1>
-      <h1>3 Youtube subscribers | 10/31/24</h1>
-      <h1>17,500 Instagram followers| 10/31/24 | next goal: 50,000</h1>
-      <h1>228 TikTok followers | 10/31/24 | next goal: 100,000</h1>
-      <h1>5 N JLPT level</h1> 
-      <h1>0 times contracted incurable Hepatitis B | 10/31/24</h1>
-      <h1>0 times contracted incurable Herpes | 10/31/24</h1>
-      <h1>0 times contracted incurable HIV | 10/31/24</h1>
-      <h1>0 times contracted incurable HPV | 10/31/24</h1>
-      <h1>28 years old</h1>
-      <h1>5'7“ height | 10/31/24</h1>
+      
+      <p><span class='stat'>$270,000,000 net worth</span> next goal: $300,000</p>
+      <p><span class='stat'>400</span> SoLo loans funded | 10/31/24</p>
+      <p><span class='stat'>2</span> Twitch followers | 10/31/24</p>
+      <p><span class='stat'>3</span> Youtube subscribers | 10/31/24</p>
+      <p><span class='stat'>17,500</span> Instagram followers| 10/31/24 | next goal: 50,000</p>
+      <p><span class='stat'>228</span> TikTok followers | 10/31/24 | next goal: 100,000</p>
+      <p><span class='stat'>5</span> N JLPT level</p>
+      <p><span class='stat'>0</span> times contracted incurable Hepatitis B | 10/31/24</p>
+      <p><span class='stat'>0</span> times contracted incurable Herpes | 10/31/24</p>
+      <p><span class='stat'>0</span> times contracted incurable HIV | 10/31/24</p>
+      <p><span class='stat'>0</span> times contracted incurable HPV | 10/31/24</p>
+      <p><span class='stat'>28</span> years old</p>
+      <p><span class='stat'>5'7“</span> height | 10/31/24</p>
     </div>
   );
 }
