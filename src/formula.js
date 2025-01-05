fb2e8bd0f54ae103b6e70f98e3bdcb051b84f645
Nathan Nguyen <micklebrain@gmail.com>
2025-01-05 10:54:39 -0800

    update

--- src/formula.js
+++ src/formula.js
@@ -5,23 +5,28 @@ import { Link } from "react-router-dom";
 function Formula() {
   return (
     <div>
-      <p><span class='stat-good'>$250,000,000 net worth</span> next goal: $300,000</p>
-      <p><span class='stat-good'>450</span> SoLo loans funded | 10/31/24</p>
-      <p><span class='stat-bad'>300</span> <span class='small'>/3000</span> stocks owned | 10/31/24 | next goal: 100,000</p>
-      <p><span class='stat-bad'>4</span> Twitch followers | 10/31/24</p>
-      <p><span class='stat-bad'>12</span> Youtube subscribers | 10/31/24</p>
-      <p><span class='stat-bad'>17,500</span> Instagram followers| 10/31/24 | next goal: 50,000</p>
-      <p><span class='stat-bad'>228</span> TikTok followers | 10/31/24 | next goal: 100,000</p>
+      <h1>health biomarkers ❤️</h1>
+      <p><span class='stat-good'>0</span> times contracted incurable Hepatitis B</p>
+      <p><span class='stat-good'>0</span> times contracted incurable Herpes</p>
+      <p><span class='stat-good'>0</span> times contracted incurable HIV</p>
+      <p><span class='stat-good'>0</span> times contracted incurable HPV</p>
+      <p><span class='stat-neutral'>29</span> years old</p>
+      <p><span class='stat-neutral'>5'7“</span> height</p>
+      <p><span class='stat-neutral'>20</span> pound curl</p>
+      <h1>finance 💵</h1>
+      <p><span class='stat-good'>$250,000 net worth</span> next goal: $300,000</p>
+      <p><span class='stat-good'>450</span> # SoLo loans funded</p>
+      <p><span class='stat-good'>$200,000 - $210,000</span> $ SoLo loans funded</p>
+      <p><span class='stat-bad'>341</span> <span class='small'>/3000</span> stocks owned</p>
+      <h1>social 📱</h1>
+      <p><span class='stat-bad'>12</span> Youtube subscribers</p>
+      <p><span class='stat-bad'>17,500</span> Instagram followers | next goal: 50,000</p>
+      <p><span class='stat-bad'>4</span> Twitch followers</p>
+      <p><span class='stat-bad'>228</span> TikTok followers | next goal: 1,000</p>
+      <h1>vanity</h1>
+      <p><span class='stat-neutral'>21</span> floor home</p>
       <p><span class='stat-bad'>5</span> <span class='small'>4 3 2 1</span> N JLPT level </p>
       <p><span class='stat-bad'>1</span> <span class='small'>2 3 4 5</span> TOPIK level</p>
-      <p><span class='stat-good'>0</span> times contracted incurable Hepatitis B | 10/31/24</p>
-      <p><span class='stat-good'>0</span> times contracted incurable Herpes | 10/31/24</p>
-      <p><span class='stat-good'>0</span> times contracted incurable HIV | 10/31/24</p>
-      <p><span class='stat-good'>0</span> times contracted incurable HPV | 10/31/24</p>
-      <p><span class='stat-neutral'>29</span> years old</p>
-      <p><span class='stat-neutral'>5'7“</span> height | 10/31/24</p>
-      <p><span class='stat-neutral'>21</span> floor home | 10/31/24</p>
-      <p><span class='stat-neutral'>20</span> pound curl | 10/31/24</p>
     </div>
   );
 }
