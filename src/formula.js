e524e618da1854c55d04ebcbd44257adfc1253f2
Nathan Nguyen <micklebrain@gmail.com>
2025-01-05 18:21:29 -0800

    update

--- src/formula.js
+++ src/formula.js
@@ -9,17 +9,19 @@ function Formula() {
     <div>
       <h1>health biomarkers ❤️</h1>
       <p><span class='stat-neutral'>29</span> years old</p>
+      <progress class='ageProgress' value="29" max="120"> 32% </progress>
       <p><span class='stat-neutral'>5'7“</span> height</p>
+      <p><span class='stat-good'>*10^6</span> mm3 red blood cell count</p>
       <p><span class='stat-good'>0</span> times contracted incurable Hepatitis B</p>
       <p><span class='stat-good'>0</span> times contracted incurable Herpes</p>
       <p><span class='stat-good'>0</span> times contracted incurable HIV</p>
       <p><span class='stat-good'>0</span> times contracted incurable HPV</p>
-      <p><span class='stat-good'>*10^6</span> mm3 Red Blood Cell count</p>
       <p><span class='stat-neutral'>20</span> pound curl</p>
       <h1>finance 💵</h1>
-      <p><span class='stat-good'>$250,000 net worth</span> next goal: $300,000</p>
+      <p><span class='stat-good'>$250,000 </span> net worth | next goal: $300,000</p>
       <p><span class='stat-good'>450</span> # SoLo loans funded</p>
       <p><span class='stat-good'>$200,000 - $210,000</span> $ SoLo loans funded</p>
+      <p><span class='stat-good'>$25,000</span> SoLo tips</p>
       <p><span class='stat-bad'>341</span> <span class='small'>/ 3000</span> stocks owned</p>
       <h1>social 📱</h1>
       <p><span class='stat-bad'>12</span> Youtube subscribers</p>
