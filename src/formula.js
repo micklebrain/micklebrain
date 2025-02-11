ae1c4603141f9568fb6a583fa5e5657419bd3c1e
Nathan Nguyen <micklebrain@gmail.com>
2025-02-11 09:48:11 -0800

    update

--- src/formula.js
+++ src/formula.js
@@ -602,7 +602,7 @@ function Formula() {
         <h3>stocks owned</h3>
         <div class="progress-bg">
           <div class="progress-bar">
-            <h3 class="raised">352&nbsp;</h3>
+            <h3 class="raised">354&nbsp;</h3>
           </div>
 
           <h3 class="goal">Goal: 6,837</h3>
