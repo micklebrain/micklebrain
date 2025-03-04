e526c8856c247a2f615d410bdfc0360d81014daa
Nathan Nguyen <micklebrain@gmail.com>
2025-03-04 13:56:47 -0800

    update

--- src/formula.js
+++ src/formula.js
@@ -635,7 +635,7 @@ function Formula() {
           </div>
           <div class="box__body">
             <div class="stats stats--main">
-              <div class="stats__amount">$4,428.78</div>
+              <div class="stats__amount">$4,450.44</div>
               <div class="stats__caption">realized P&L</div>
             </div>
 
