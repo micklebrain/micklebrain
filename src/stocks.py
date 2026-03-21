9c54d1ee54aacb6c40f43052e5a525c634a7c01b
Nathan Nguyen <micklebrain@gmail.com>
2026-03-21 11:55:29 +0800

    u

--- src/stocks.py
+++ src/stocks.py
@@ -140,7 +140,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 
 # upcoming splits - 
 # upcoming delisting - 
-stocksToAdd = ["MELI"]
+stocksToAdd = ["VCX"]
 
 for stock in stocksToAdd:
     addStock(stock, ownWebull=True)
