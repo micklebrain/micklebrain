e9ae936a6ff1738e45f84a8a0ac40c28f63f40a3
Nathan Nguyen <micklebrain@gmail.com>
2026-03-24 23:05:03 +0800

    u

--- src/stocks.py
+++ src/stocks.py
@@ -140,7 +140,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 
 # upcoming splits - 
 # upcoming delisting - 
-stocksToAdd = [""]
+stocksToAdd = ["CZFS"]
 
 for stock in stocksToAdd:
-    addStock(stock, ownWebull=True)
+    addStock(stock, ownEtrade=True)
