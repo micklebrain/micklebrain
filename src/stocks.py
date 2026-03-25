61c7190c0da19f834d8847006ae30e69833f03ab
Nathan Nguyen <micklebrain@gmail.com>
2026-03-25 23:58:11 +0800

    u

--- src/stocks.py
+++ src/stocks.py
@@ -140,7 +140,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 
 # upcoming splits - 
 # upcoming delisting - 
-stocksToAdd = ["MLKN"]
+stocksToAdd = ["JCAP"]
 
 for stock in stocksToAdd:
     addStock(stock, ownEtrade=True)
