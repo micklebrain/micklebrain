1fbfcd16c3d106627a6a934986b6b765a52fa98d
Nathan Nguyen <micklebrain@gmail.com>
2026-04-24 01:28:29 +0800

    u

--- src/stocks.py
+++ src/stocks.py
@@ -141,7 +141,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = [""]
+stocksToAdd = ["DBVT"]
 
 for stock in stocksToAdd:
     addStock(stock, ownEtrade=True)
