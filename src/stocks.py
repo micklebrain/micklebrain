32787b4e383b36f74719a141dd5a7bacb00361ad
Nathan Nguyen <micklebrain@gmail.com>
2026-05-07 01:59:44 +0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -141,7 +141,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["FCNCA"]
+stocksToAdd = ["E"]
 
 for stock in stocksToAdd:
-    addStock(stock, ownWebull=True)
+    addStock(stock, ownEtrade=True)
