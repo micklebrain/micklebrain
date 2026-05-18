a4bc19a86e0f4ccc2a1c5b40d35a7e52aa304494
Nathan Nguyen <micklebrain@gmail.com>
2026-05-19 01:39:21 +0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -141,7 +141,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["WTW"]
+stocksToAdd = ["KLAC"]
 
 for stock in stocksToAdd:
     addStock(stock, ownWebull=True)
