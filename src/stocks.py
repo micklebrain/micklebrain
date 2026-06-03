0e40236059d1e0b15e8ccb6d9c721cec26efa61f
Nathan Nguyen <micklebrain@gmail.com>
2026-06-03 09:40:23 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -155,7 +155,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["8729"]
+stocksToAdd = ["MRVL"]
 
 for stock in stocksToAdd:
-    addStock(stock, ownInteractiveBrokers=True)
+    addStock(stock, ownWebull=True)
