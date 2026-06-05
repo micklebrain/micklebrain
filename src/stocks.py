bdcf9e901c51f73e231218b084e525c640e14e7c
Nathan Nguyen <micklebrain@gmail.com>
2026-06-04 17:39:50 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -155,7 +155,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["003490"]
+stocksToAdd = ["003550"]
 
 for stock in stocksToAdd:
     addStock(stock, ownInteractiveBrokers=True)
