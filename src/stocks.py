b43d32390bb9207f4d6956594c91f4b18b40b214
Nathan Nguyen <micklebrain@gmail.com>
2026-06-06 02:10:05 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -155,7 +155,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["CTSH"]
+stocksToAdd = [""]
 
 for stock in stocksToAdd:
     addStock(stock, ownEtrade=True)
