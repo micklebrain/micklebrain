9b278c9ad11bcf4e5fe92c5bb82b946440f50048
Nathan Nguyen <micklebrain@gmail.com>
2026-06-21 15:34:32 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["CGBD"]
+stocksToAdd = ["CNOB"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Carlyle Secured Lending Inc", dividend=True)
+    addStock(stock, name="ConnectOne Bancorp Inc", dividend=True)
