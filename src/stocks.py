34517f5926a591c417eb3934b7dd72766ff58a17
Nathan Nguyen <micklebrain@gmail.com>
2026-06-22 00:49:51 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["DGICA"]
+stocksToAdd = ["DMLP"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Donegal Group Inc Class A", dividend=True)
+    addStock(stock, name="Dorchester Minerals LP", dividend=True)
