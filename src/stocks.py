c9a5a55bdf0d3ca3eb8830849e16ff1db0dd7023
Nathan Nguyen <micklebrain@gmail.com>
2026-06-22 15:51:51 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["ESPN"]
+stocksToAdd = ["ESQ"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Epsilon Energy Ltd", dividend=True)
+    addStock(stock, name="Esquire Financial Holdings Inc", dividend=True)
