b399adbbe4551a407376effcc4d1e8c5f3b7aabd
Nathan Nguyen <micklebrain@gmail.com>
2026-06-22 18:10:16 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["INDB"]
+stocksToAdd = ["KELYA"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Independent Bank Corp (Massachusetts)", dividend=True)
+    addStock(stock, name="Kelly Services Inc Class A", dividend=True)
