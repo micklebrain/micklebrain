1dfece060986ec7680e3a200f2fbb7d39850f0b0
Nathan Nguyen <micklebrain@gmail.com>
2026-06-23 00:43:41 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["LSTR"]
+stocksToAdd = ["MLCI"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Landstar System Inc", dividend=True)
+    addStock(stock, name="Mount Logan Capital Inc", dividend=True)
