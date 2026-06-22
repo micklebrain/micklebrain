6c7041e40a927086044acebcba6e8115087a1442
Nathan Nguyen <micklebrain@gmail.com>
2026-06-22 17:14:38 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["GPC"]
+stocksToAdd = ["GWRS"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Genuine Parts Co", dividend=True)
+    addStock(stock, name="Global Water Resources Inc", dividend=True)
