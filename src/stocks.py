8503436aa54d9add3b69b935dc62887559cfe324
Nathan Nguyen <micklebrain@gmail.com>
2026-06-22 17:55:15 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["GWRS"]
+stocksToAdd = ["INDB"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Global Water Resources Inc", dividend=True)
+    addStock(stock, name="Independent Bank Corp (Massachusetts)", dividend=True)
