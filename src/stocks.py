660e284775b51415c883429bcb724e86748715d6
Nathan Nguyen <micklebrain@gmail.com>
2026-06-24 00:18:17 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["QNT"]
+stocksToAdd = ["BSY"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Quantinuum Inc", ownEtrade=True)
+    addStock(stock, name="Bentley Systems, Inc", ownEtrade=True)
