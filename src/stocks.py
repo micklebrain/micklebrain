7015469cf19852267d3ef62b2a48c5854171bb8a
Nathan Nguyen <micklebrain@gmail.com>
2026-06-24 16:47:05 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["PLYX"]
+stocksToAdd = ["ARXS"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Polaryx Therapeutics, Inc")
+    addStock(stock, name="Arxis Inc")
