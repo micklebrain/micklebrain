abe3a863a82e785a9559ef58d083f0db21314d80
Nathan Nguyen <micklebrain@gmail.com>
2026-06-24 15:25:02 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["BSY"]
+stocksToAdd = ["GENB"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Bentley Systems, Inc")
+    addStock(stock, name="Generate Biomedicines Inc")
