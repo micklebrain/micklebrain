1c82ffe8f628e2d62e0add1f608b45607e0de7d8
Nathan Nguyen <micklebrain@gmail.com>
2026-06-24 23:09:31 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["ARXS"]
+stocksToAdd = ["WLTH"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Arxis Inc")
+    addStock(stock, name="Wealthfront Corp", ownEtrade=True)
