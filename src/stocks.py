14d33e9f5b59451ef5e7e40e7489979aa308f96b
Nathan Nguyen <micklebrain@gmail.com>
2026-06-25 12:46:49 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["SAIA"]
+stocksToAdd = ["ADI"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Saia Inc")
+    addStock(stock, name="Analog Devices Inc")
