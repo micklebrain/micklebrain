0cc7022944652e294de16bcbee7a6c6ef6d36c40
Nathan Nguyen <micklebrain@gmail.com>
2026-06-26 16:09:41 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["PKN"]
+stocksToAdd = ["ZAL"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Orlen SA")
+    addStock(stock, name="Zalando SE", ownInteractiveBrokers=True)
