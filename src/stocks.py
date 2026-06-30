4e05883e0615c8d2bb382c0d002d182166e7134f
Nathan Nguyen <micklebrain@gmail.com>
2026-06-30 16:47:56 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["DNP"]
+stocksToAdd = ["4IG"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Dino Polska SA", ownInteractiveBrokers=True)
+    addStock(stock, name="4ig Plc.", ownInteractiveBrokers=True)
