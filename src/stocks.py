13aad30a8ae16308124f96673d71da21ddc28927
Nathan Nguyen <micklebrain@gmail.com>
2026-06-30 16:33:01 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["DMC"]
+stocksToAdd = ["DNP"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Del Monte Corp", ownWebull=True)
+    addStock(stock, name="Dino Polska SA", ownInteractiveBrokers=True)
