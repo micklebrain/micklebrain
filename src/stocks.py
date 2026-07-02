5d32eaabb709ea379ee7b585b25e4336c5677e66
Nathan Nguyen <micklebrain@gmail.com>
2026-07-02 16:11:37 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["MBGL"]
+stocksToAdd = ["AF"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Mobility Global Inc", ownWebull=True)
+    addStock(stock, name="Air France KLM SA", ownInteractiveBrokers=True)
