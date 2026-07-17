08e6f4bd25ac86f8b7e9563b448645777c040f04
Nathan Nguyen <micklebrain@gmail.com>
2026-07-17 00:04:02 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["SITM"]
+stocksToAdd = ["CDI"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="SiTime Corp", ownEtrade=True)
+    addStock(stock, name="Christian Dior SE", ownInteractiveBrokers=True)
