7c6bb863a353a60ce16d87c4a099d53bf6ca9f3f
Nathan Nguyen <micklebrain@gmail.com>
2026-07-17 07:44:28 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["CDI"]
+stocksToAdd = ["CSQR"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Christian Dior SE", ownInteractiveBrokers=True)
+    addStock(stock, name="Csquare Inc", ownEtrade=True)
