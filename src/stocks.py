66f22293deee1c220d006d84c319f0ef286885b5
Nathan Nguyen <micklebrain@gmail.com>
2026-07-20 12:33:09 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["STDN"]
+stocksToAdd = ["JMKE"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Standard Nuclear Inc", ownEtrade=True)
+    addStock(stock, name="Jersey Mike's Subs")
