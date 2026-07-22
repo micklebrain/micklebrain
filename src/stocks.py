b194fea8d1f0b1f77cf6476c8115e8a59e80044b
Nathan Nguyen <micklebrain@gmail.com>
2026-07-22 07:47:16 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["JMKE"]
+stocksToAdd = ["LIME"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Jersey Mike's Subs")
+    addStock(stock, name="Neutron Holdings Inc")
