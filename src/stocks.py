8fd215fe3d1855d310b2a3c22fb302dbc5670eb3
Nathan Nguyen <micklebrain@gmail.com>
2026-07-28 14:38:10 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["005935"]
+stocksToAdd = ["GVH"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Samsung Electronics Co Ltd", ownInteractiveBrokers=True)
+    addStock(stock, name="Globavend Holdings Ltd")
