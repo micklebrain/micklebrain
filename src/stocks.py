faa3aad83445e135363d62b46363c94f48ec72d7
Nathan Nguyen <micklebrain@gmail.com>
2026-07-28 14:54:33 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["BHST"]
+stocksToAdd = ["HITI"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Bioharvest Sciences Inc")
+    addStock(stock, name="High Tide Inc")
