d747355c0ffd38bab61c5fd264039dee79d37e2c
Nathan Nguyen <micklebrain@gmail.com>
2026-07-28 16:58:45 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["BTQ"]
+stocksToAdd = ["AEC"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="BTQ Technologies Corp")
+    addStock(stock, name="Anfield Energy Inc")
