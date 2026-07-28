7e0568526b1262dc7a93a5d6feb0a8e01059af51
Nathan Nguyen <micklebrain@gmail.com>
2026-07-28 14:52:20 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["GVH"]
+stocksToAdd = ["BHST"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Globavend Holdings Ltd")
+    addStock(stock, name="Bioharvest Sciences Inc")
