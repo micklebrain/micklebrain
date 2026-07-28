bd76408749abe8896f84201ff097355b00185fbf
Nathan Nguyen <micklebrain@gmail.com>
2026-07-28 15:48:35 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["NEXM"]
+stocksToAdd = ["RADX"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="NexMetals Mining Corp")
+    addStock(stock, name="Radiopharm Theranostics Limited")
