998d3ab5020b1b836a1c932239663b2450bfeaa3
Nathan Nguyen <micklebrain@gmail.com>
2026-07-29 07:24:41 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["HSLV"]
+stocksToAdd = ["IREN"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Highlander Silver Corp")
+    addStock(stock, name="IREN Ltd")
