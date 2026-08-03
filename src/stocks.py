70abbe4d2c01f3a83c7518230a6add5a32297c76
Nathan Nguyen <micklebrain@gmail.com>
2026-08-02 18:23:13 -0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["BHM"]
+stocksToAdd = ["PSIX"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Bluerock Homes Trust Inc")
+    addStock(stock, name="Power Solutions International Inc")
