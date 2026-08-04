72ccf1f2e9599b8745772e9f3fea708324e26edd
Nathan Nguyen <micklebrain@gmail.com>
2026-08-05 00:51:14 +0900

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["EVAX"]
+stocksToAdd = ["IXHL"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Evaxion A/S", ownEtrade=True)
+    addStock(stock, name="Incannex Healthcare Inc", ownEtrade=True)
