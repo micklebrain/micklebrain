1fb7c18ddff5b22f5f0050e7a86fdc7a8de36c37
Nathan Nguyen <micklebrain@gmail.com>
2026-08-09 11:55:49 +0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["EDSA"]
+stocksToAdd = ["NVA"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Edesa Biotech Inc")
+    addStock(stock, name="Nova Minerals Corp")
