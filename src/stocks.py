bc5efefa4bd8f5ebc1b3ad8db082f35383e8b22c
Nathan Nguyen <micklebrain@gmail.com>
2026-08-09 11:24:22 +0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -162,7 +162,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["DPRO"]
+stocksToAdd = ["EDSA"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Draganfly Inc")
+    addStock(stock, name="Edesa Biotech Inc")
