c79e93878d2334d579e1c288afe5f122b6cc1a1f
Nathan Nguyen <micklebrain@gmail.com>
2026-08-26 14:51:30 +0700

    u

--- src/stocks.py
+++ src/stocks.py
@@ -166,7 +166,7 @@ print(f"{len(printedResults)} / {totalNumberStocks} stocks | {percentageComplete
 # upcoming splits -
 # upcoming delisting -
 # upcoming spin offs -
-stocksToAdd = ["DCBO"]
+stocksToAdd = ["RMS"]
 
 for stock in stocksToAdd:
-    addStock(stock, name="Docebo Inc")
+    addStock(stock, name="Hermès International S.C.A.")
